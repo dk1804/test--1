5dffebc0a013afcdffff08867b453a5fb4597a1b
dk1804 <15229292302@163.com>
2020-10-11 21:32:31 +0800

    猜凶手代码

---
+++ 猜凶手/猜凶手/test.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//void Find_Killer()
+//{
+//	char killer = 0;
+//	for (killer = 'A'; killer < 'D'; killer++)
+//	{
+//		if (((killer != 'A') + (killer == 'C') + (killer == 'D') + (killer != 'D')) == 3 )
+//		{
+//			printf("%c is killer\n", killer);
+//		}
+//	}
+//}
+//int main()
+//{
+//	Find_Killer();
+//	return 0;
+//}
+#include<stdio.h>
+void Rank()
+{
+int A, B, C, D ,E= 0;
+if ((A == 3) ^ (B == 2) + ()
+}
+int main()
+{
+	
+	Rank();
+	return 0;
+}
